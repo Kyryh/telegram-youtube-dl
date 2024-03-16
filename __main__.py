645afc8a197b98b0d92267724ba3a3035ac61acd
Kyryh <kyrykiller07@gmail.com>
2024-03-16 21:36:39 +0100

    Added video and audio downloading

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import logging
 
-from telegram import ForceReply, Update, MessageEntity
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update, MessageEntity, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 from yt_dlp import YoutubeDL
 from os import getenv
 
@@ -29,19 +29,77 @@ async def not_allowed(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def handle_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for url in update.effective_message.parse_entities([MessageEntity.URL, MessageEntity.TEXT_LINK]).values():
-        await try_download(url, update.effective_user.id, context)
+        await show_download_options(url, update.effective_chat.id, context)
 
-async def try_download(link: str, user_id: int, context: ContextTypes.DEFAULT_TYPE):
-    # do stuff
-    pass
+async def show_download_options(url: str, chat_id: int, context: ContextTypes.DEFAULT_TYPE):
+
+    with YoutubeDL() as ydl:
+        video_info = ydl.extract_info(url, download=False)
+
+    params = {
+        "thumbnail": video_info['thumbnail'],
+        "duration": video_info['duration'],
+        "url": url,
+        "audio": False
+    }
+
+    keyboard = [
+        [
+            InlineKeyboardButton("Video, highest quality", callback_data=params | {
+                "ytdl_options": {
+                    
+                }
+            })
+        ],
+        [
+            InlineKeyboardButton("Video, lowest filesize", callback_data=params | {
+                "ytdl_options": {
+                    "format_sort": ["+size","+br","+res","+fps"]
+                }
+            })
+        ],
+        [
+            InlineKeyboardButton("Audio", callback_data=params | {
+                "ytdl_options": {
+                    "format": "bestaudio",
+                    "postprocessors": [
+                        {
+                            "key": "FFmpegExtractAudio",
+                            "preferredcodec": "wav"
+                        }
+                    ]
+                },
+                "audio": True
+            })
+        ]
+    ]
+    
+
+    await context.bot.send_photo(
+        chat_id=chat_id,
+        photo=video_info['thumbnail'],
+        caption=video_info['title'],
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+
+
+async def try_download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    data = update.callback_query.data
+
+    data["ytdl_options"]["outtmpl"] = "temp"
+    with YoutubeDL(data["ytdl_options"]) as ydl:
+        download_result = ydl.extract_info(data["url"])
+        filename = "temp." + ("wav" if data["audio"] else download_result["ext"])
+
 
 def main():
-    application = Application.builder().token(getenv("TOKEN")).build()
+    application = Application.builder().token(getenv("TOKEN")).arbitrary_callback_data(True).build()
 
     application.add_handler(MessageHandler(~filters.User(ALLOWED_USER_IDS), not_allowed))
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.Entity(MessageEntity.URL) | filters.Entity(MessageEntity.TEXT_LINK), handle_links))
-
+    application.add_handler(CallbackQueryHandler(try_download))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
